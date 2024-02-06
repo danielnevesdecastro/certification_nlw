abd90b2aaee5a21aaba525e2fb964c732837a7d1
Daniel Neves <danielnevesdecastro@gmail.com>
2024-02-05 22:54:08 -0300

    first commit

---
+++ src/main/java/br/com/neves/cetification/CetificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.neves.cetification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CetificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CetificationApplication.class, args);
+	}
+
+}
